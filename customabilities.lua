9dd7bf2d67143f1ef9e69d2f5d43e43378401af3
NaturalNi13 <naturalidiot1234@gmail.com>
2024-10-29 18:50:22 -0500

    Create customabilities.lua

---
+++ customabilities.lua
@@ -0,0 +1,2 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/NaturalNi13/Abilities/refs/heads/main/hammerbounce.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/NaturalNi13/Abilities/refs/heads/main/creamglide.lua"))()
