08a22fabb3aea3cf15ed98c613897af8bac793d6
NaturalNi13 <naturalidiot1234@gmail.com>
2024-11-07 15:17:16 -0600

    Update 1customabilities.lua

--- 1customabilities.lua
+++ 1customabilities.lua
@@ -2,17 +2,4 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/NaturalNi13/Abilities
 loadstring(game:HttpGet("https://raw.githubusercontent.com/NaturalNi13/Abilities/refs/heads/main/creamglide.lua"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/NaturalNi13/Abilities/refs/heads/main/shadowrjump.lua"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/NaturalNi13/Abilities/refs/heads/main/rollability.lua"))()
--- Function to show a notification using Roblox's default corner notification
-local function showNotification(title, text, duration)
-    game.StarterGui:SetCore("SendNotification", {
-        Title = title;         -- Title of the notification
-        Text = text;           -- Body of the notification
-        Duration = duration;   -- Duration in seconds
-    })
-end
-
--- Example usage: Display a welcome message
-showNotification("Natural Idiot", "If you got this from somewhere besides my discord server dm me: naturalidiot123", 15)
-
--- You can use this function anytime to display other notifications. For example:
--- showNotification("Achievement Unlocked!", "You reached Level 10!", 3)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/NaturalNi13/Abilities/refs/heads/main/notify.lua"))()
