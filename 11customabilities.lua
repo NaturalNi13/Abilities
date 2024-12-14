1148ade1a1859c4f5984ccb62a21cbe4d8fc91a6
NaturalNi13 <naturalidiot1234@gmail.com>
2024-12-14 14:08:28 -0600

    Update 11customabilities.lua

--- 11customabilities.lua
+++ 11customabilities.lua
@@ -1,2 +1,9 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/NaturalNi13/AbilitiesE/refs/heads/main/notify.lua"))()
 
+--ABilities
+loadstring(game:HttpGet("https://raw.githubusercontent.com/NaturalNi13/AbilitiesE/refs/heads/main/cglide.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/NaturalNi13/AbilitiesE/refs/heads/main/hbounce.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/NaturalNi13/AbilitiesE/refs/heads/main/rjump.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/NaturalNi13/AbilitiesE/refs/heads/main/sdash.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/NaturalNi13/AbilitiesE/refs/heads/main/sroll.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/NaturalNi13/AbilitiesE/refs/heads/main/tretreat.lua"))()
