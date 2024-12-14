d82a86bdf8de7c077e98497de7b157aa68e47595
NaturalNi13 <naturalidiot1234@gmail.com>
2024-12-14 14:01:37 -0600

    Update 11customabilities.lua

--- 11customabilities.lua
+++ 11customabilities.lua
@@ -1,7 +1,2 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/NaturalNi13/Abilities/refs/heads/main/notify.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/NaturalNi13/AbilitiesE/refs/heads/main/notify.lua"))()
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/NaturalNi13/Abilities/refs/heads/main/hammerbounce.lua"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/NaturalNi13/Abilities/refs/heads/main/creamglide.lua"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/NaturalNi13/Abilities/refs/heads/main/shadowrjump.lua"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/NaturalNi13/Abilities/refs/heads/main/rollability.lua"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/NaturalNi13/AbilitiesE/refs/heads/main/ktsdash.lua"))()
